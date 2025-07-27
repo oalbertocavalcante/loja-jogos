c2ddcd4bbc37f0d38cec9f8cfe1bd9923a0e790c
Alberto Cavalcante <232014610@aluno.unb.br>
2025-07-26 22:23:57 -0300

    Primeiro commit - projeto loja-jogos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
+import { CadastroProdutoComponent } from './componentes/cadastro-produto/cadastro-produto.component';
+
+const routes: Routes = [
+  { path: 'painel-principal', component: PainelPrincipalComponent },
+  { path: 'cadastro-produto', component: CadastroProdutoComponent },
+  { path: 'cadastro-produto/:id', component: CadastroProdutoComponent },
+  { path: '', redirectTo: '/painel-principal', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
